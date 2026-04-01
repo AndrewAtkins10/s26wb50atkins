05687a7a4b3c434f92d4b2a9d03d37425788c105
Andrew Atkins <S551050@nwmissouri.edu>
2026-04-01 14:30:27 -0500

    fixing part 7 and 8

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var unicyclesRouter = require('./routes/unicycles');
-
+var unicyclesRouter = require('./routes/unicycles'); // Added for Part 5
+var gridRouter = require('./routes/grid');           // Added for Part 7
 var app = express();
 
 // view engine setup
@@ -21,9 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/gridbuild', gridRouter);
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
